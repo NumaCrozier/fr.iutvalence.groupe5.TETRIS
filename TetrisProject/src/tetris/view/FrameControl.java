1096900d2d73d40f1f46609478dd26c76dc67815
Sedara <axel.sedara@gmail.com>
2015-06-09 16:40:05 +0200

    Affichage de l'écran de jeu

--- TetrisProject/src/tetris/view/FrameControl.java
+++ TetrisProject/src/tetris/view/FrameControl.java
@@ -14,6 +14,10 @@ import javax.swing.SwingConstants;
 import tetris.controller.ConfigManager;
 import tetris.view.containers.LabelControl;
 
+/**
+ * @author Sedara
+ *
+ */
 public class FrameControl extends JFrame implements KeyListener, WindowListener{
 
 	private static final long serialVersionUID = 1L;
